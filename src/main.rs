6beb2df5872aa6cd074b8819926483f1f8acb460
Christopher Wells <cwellsny@nycap.rr.com>
2023-07-14 17:49:17 -0700

    Re-add progressbar and write results to file

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ use std::{
     collections::{BTreeMap, BTreeSet},
     fs::File,
     io,
-    io::BufRead,
+    io::{BufRead, Write},
 };
 
 use bit_set::BitSet;
-use indicatif::ProgressIterator;
-use rayon::prelude::*;
+use indicatif::{ParallelProgressIterator, ProgressIterator};
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
 fn main() {
     println!("Reading in words file...");
@@ -22,24 +22,13 @@ fn main() {
     println!("Building up the graph...");
     let graph = Graph::build_from_words(&words);
 
+    println!("Searching for five-cliques...");
     let cliques = graph.search_for_clique_non_recursive();
-    for clique in cliques.iter() {
-        println!("Found a clique:");
-        for word in clique.iter() {
-            println!("{}", word);
-        }
+    println!("Found {} five-cliques.", cliques.len());
 
-        println!("\nletters:");
-        let mut letters: Vec<char> = clique
-            .iter()
-            .flat_map(|word| word.chars().collect::<Vec<char>>())
-            .collect();
-        letters.sort();
-        for l in letters.iter() {
-            print!("{}", l);
-        }
-        println!();
-    }
+    let output_filepath = "five_cliques.csv";
+    write_cliques_to_file(output_filepath, &cliques).unwrap();
+    println!("Wrote results to: {}", output_filepath);
 }
 
 type Word = String;
@@ -48,6 +37,33 @@ type Words = Vec<(Word, BTreeSet<char>)>;
 type Clique = BTreeSet<Word>;
 
 type WordIndexSet = BitSet;
+
+fn write_cliques_to_file(filepath: &str, cliques: &BTreeSet<Clique>) -> io::Result<()> {
+    let mut output_file = File::create(filepath).unwrap();
+    writeln!(
+        output_file,
+        "word_1,word_2,word_3,word_4,word_5,missing_letter"
+    )?;
+    for clique in cliques.iter() {
+        for word in clique.iter() {
+            write!(output_file, "{},", word)?;
+        }
+
+        let mut letters: Vec<char> = clique
+            .iter()
+            .flat_map(|word| word.chars().collect::<Vec<char>>())
+            .collect();
+        letters.sort();
+        for l in 'a'..='z' {
+            if !letters.contains(&l) {
+                write!(output_file, "{}", l)?;
+            }
+        }
+        writeln!(output_file)?;
+    }
+
+    Ok(())
+}
 
 struct Graph {
     edges: BTreeMap<WordIndex, WordIndexSet>,
@@ -86,6 +102,7 @@ impl Graph {
             .iter()
             .collect::<Vec<WordIndex>>()
             .par_iter()
+            .progress_count(neighbors_0.len() as u64)
             .flat_map(|word_index| self.search_for_clique_with_word(*word_index, &neighbors_0))
             .collect()
     }
@@ -135,7 +152,6 @@ impl Graph {
                             &self.words[word_3],
                             &self.words[word_4],
                         ]);
-                        println!("{:?}", current);
 
                         solutions.insert(current);
                     }
